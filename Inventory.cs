12e007415759dbd2b59ecb21a27e6417523a04a9
Ryan Lawrence <Beherenow119@gmail.com>
2024-10-25 12:37:52 -0600

    Adds Binding List test data to the Products and Parts Data Grids.

---
+++ Inventory.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel;
+
+namespace Products_and_Parts
+{
+    internal class Inventory
+    {
+        public static BindingList<Product> Product { get; set; } = new BindingList<Product>();
+        public static BindingList<Part> AllParts { get; set; } = new BindingList<Part>();
+
+        static Inventory()
+        {
+            Product.Add(new Product(0, "Acer Laptop", 899.99m, 10, 2, 20));
+            Product.Add(new Product(1, "Vizio Television", 999.99m, 8, 1, 10));
+            Product.Add(new Product(2, "Apple iPhone 18", 799.99m, 6, 4, 15));
+
+            AllParts.Add(new InHouse(0, "Power Supply", 299.99m, 15, 8, 30, 00100));
+            AllParts.Add(new InHouse(1, "Battery", 49.99m, 3, 1, 5, 00200));
+            AllParts.Add(new Outsourced(2, "Glass Display", 199.99m, 5, 1, 6, "Apple"));
+            AllParts.Add(new Outsourced(3, "Wireless Mouse", 79.99m, 5, 2, 8, "Logitech"));
+            AllParts.Add(new Outsourced(4, "Charger Cable", 29.99m, 11, 20, 10, "Anker"));
+        }
+
+        public static void AddProduct(Product product)
+        {
+
+        }
+
+        public static bool RemoveProduct(int productID)
+        {
+            return true;
+        }
+
+        public static Product LookupProduct(int productID)
+        {
+            return null;
+        }
+
+        public static void UpdateProduct(int productID, Product product)
+        {
+
+        }
+
+        public static void AddPart(Part part)
+        {
+
+        }
+
+        public static bool DeletePart(Part part)
+        {
+            return true;
+        }
+
+        public static Part LookupPart(int partID)
+        {
+            return null;
+        }
+
+        public static void UpdatePart(int partID, Part part)
+        {
+
+        }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+
+namespace Products_and_Parts
+{
+    internal class Product
+    {
+        public static BindingList<Part> AssociatedParts = new BindingList<Part>();
+        public int ProductID { get; set; }  
+        public string Name { get; set; }    
+        public decimal Price { get; set; }
+        public int InStock { get; set; }    
+        public int Min {  get; set; }
+        public int Max { get; set; }
+
+        public static BindingList<Product> testProductData { get; set; } = new BindingList<Product>();
+
+        // A static constructor for a one-time initialization of test data (will only generate once and not on every class instance)
+
+        public Product(int productID, string name, decimal price, int inStock, int min, int max)
+        {
+            ProductID = productID;
+            Name = name;
+            Price = price;
+            InStock = inStock;
+            Min = min;
+            Max = max;
+        }
+
+        public static void AddAssociatedPart(Part part)
+        {
+
+        }
+
+        public static bool RemoveAssociatedPart(int partID)
+        {
+            return true;
+        }
+
+        public static Part LookupAssociatedPart(int partID)
+        {
+            return null;
+        }
+    }
+}
